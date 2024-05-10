8ba5212363e05881af3b67e4396fcaecab7d2a30
pavel-stastny <pavel.stastny@gmail.com>
2024-05-10 17:44:46 +0200

    Prepare configurations for apache proxy

--- channel/start-apache.sh
+++ channel/start-apache.sh
@@ -70,11 +70,81 @@ if [ ! -f "/usr/local/etc/ssl/hostname.txt" ] || [ "$(cat /usr/local/etc/ssl/hos
 	openssl genrsa -out "$CLIENT_DIR/ceskadigitalniknihovna.cz.key" 2048
 	openssl req -new -nodes -sha256 -key "$CLIENT_DIR/ceskadigitalniknihovna.cz.key" -out "$CLIENT_DIR/ceskadigitalniknihovna.cz.csr" -subj "/C=CZ/ST=Czech republic/CN=czechdigitallibrary.cz"  -addext "subjectAltName = DNS:czechdigitallibrary.cz"
 	openssl x509 -req -in "$CLIENT_DIR/ceskadigitalniknihovna.cz.csr"  -CA "$CA_DIR/kramerius-ca.crt" -CAkey "$CA_DIR/kramerius-ca.key" -CAcreateserial -out "$CLIENT_DIR/ceskadigitalniknihovna.cz.crt" -days 5475 -sha256 -extfile "$CLIENT_DIR/ceskadigitalniknihovna.cz.ext" -passin env:CA_PASSWORD
+	cat "$CLIENT_DIR/ceskadigitalniknihovna.cz.crt" > "$CLIENT_DIR/cdk-proxy.crt"
+	cat "$CLIENT_DIR/ceskadigitalniknihovna.cz.key" >> "$CLIENT_DIR/cdk-proxy.crt"
+	
 
 	
 	#Zip file 
 	zip -j "$ZIP_DIR/certificates.zip" "$CLIENT_DIR/ceskadigitalniknihovna.cz.crt" "$CLIENT_DIR/ceskadigitalniknihovna.cz.key" "$CA_DIR/kramerius-ca.crt"
 
+	#Mkdir 
+    mkdir "/usr/local/apache2/conf/cdk-auth.$CDK_HOSTNAME"
+	
+	#Configuration
+	printf "<VirtualHost *:80>\n\
+    ServerAdmin admin@cdk-auth.$CDK_HOSTNAME\n\
+    ServerName cdk-auth.$CDK_HOSTNAME\n\
+    ServerAlias cdk-auth.$CDK_HOSTNAME\n\
+    DocumentRoot /var/www/cdk-auth.$CDK_HOSTNAME\n\
+    ErrorLog \${APACHE_LOG_DIR}/error.log\n\
+    CustomLog \${APACHE_LOG_DIR}/access.log combined\n\
+	\n\
+    RewriteEngine on\n\
+    RewriteCond %%{SERVER_NAME} =cdk-auth.$CDK_HOSTNAME\n\
+    RewriteRule ^ https://%%{SERVER_NAME}%%{REQUEST_URI} [END,NE,R=permanent]\n\
+	</VirtualHost>\n" > "/usr/local/apache2/conf/cdk-auth.$CDK_HOSTNAME/cdk-auth.$CDK_HOSTNAME.conf"		
+		
+
+	printf "<IfModule mod_ssl.c>\n\
+	<VirtualHost *:443>\n\
+    ServerAdmin admin@cdk-auth.$CDK_HOSTNAME\n\
+    ServerName cdk-auth.$CDK_HOSTNAME\n\
+    ServerAlias cdk-auth.$CDK_HOSTNAME\n\
+    DocumentRoot /var/www/cdk-auth.$CDK_HOSTNAME\n\
+    ErrorLog \${APACHE_LOG_DIR}/error.log\n\
+    CustomLog \${APACHE_LOG_DIR}/access.log combined\n\
+	\n\
+    <LocationMatch \"^/search/(.*)$\">\n\
+      SSLOptions +StdEnvVars\n\
+	\n\
+      <IfModule mod_proxy.c>\n\
+        ProxyPreserveHost On\n\
+		\n\
+        ProxyPass \"https://127.0.0.1:8443/search/\$1\" retry=5\n\
+        ProxyPassReverse \"https://127.0.0.18443/search/\$1\"\n\
+		\n\
+        <RequireAll>\n\
+          <RequireAny>\n\
+            Require expr %%{SSL_CLIENT_S_DN_CN} == \"ceskadigitalniknihovna.cz\"\n\
+            Require expr %%{SSL_CLIENT_S_DN_CN} == \"czechdigitallibrary.cz\"\n\
+          </RequireAny>\n\
+        </RequireAll>\n\
+      </IfModule>\n\
+    </LocationMatch>\n\
+	\n\
+    <Directory /var/www/k7.inovatika.dev>\n\
+        Options Indexes FollowSymLinks\n\
+        AllowOverride All\n\
+        Require all granted\n\
+    </Directory>\n\
+	\n\
+    SSLEngine On\n\
+    SSLUseStapling Off\n\
+    SSLVerifyClient require\n\
+    SSLVerifyDepth 10\n\
+	\n\
+    SSLCertificateFile /TODO_PATH/ssl/certs/cdk-auth.$CDK_HOSTNAME.crt\n\
+    SSLCertificateKeyFile /TODO_PATH/ssl/certs/cdk-auth.$CDK_HOSTNAME.key\n\
+    SSLCACertificateFile /TODO_PATH/ssl/ca/kramerius-ca.crt\n\
+    SSLProxyEngine on\n\
+	\n\
+    SSLProxyMachineCertificateFile /TODO_PATH/ssl/client/cdk-proxy.crt\n\
+	\n\
+	</VirtualHost>\n\
+	</IfModule>\n" >  "/usr/local/apache2/conf/cdk-auth.$CDK_HOSTNAME/cdk-auth.${CDK_HOSTNAME}_ssl.conf"	
+		
+		
 fi
 
 # Spuštění Apache
