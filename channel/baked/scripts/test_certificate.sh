469ee2bdbc5b46d908fd9e5b38a55b7ef18f7079
pavel-stastny <pavel.stastny@gmail.com>
2024-10-29 13:41:28 +0100

    CA certificate path

--- channel/baked/scripts/test_certificate.sh
+++ channel/baked/scripts/test_certificate.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 export CDK_HOSTNAME=$(<../hostname.txt)
 echo "Testing client certificate: Connecting to https://cdk-auth.$CDK_HOSTNAME/"
-curl --verbose --cert ../client/ceskadigitalniknihovna.cz.crt --key ../client/ceskadigitalniknihovna.cz.key --cacert ../certs/cdk-auth.$CDK_HOSTNAME.crt   https://cdk-auth.$CDK_HOSTNAME/
+curl --verbose --cert ../client/ceskadigitalniknihovna.cz.crt --key ../client/ceskadigitalniknihovna.cz.key --cacert ../ca/kramerius-ca.crt   https://cdk-auth.$CDK_HOSTNAME/
