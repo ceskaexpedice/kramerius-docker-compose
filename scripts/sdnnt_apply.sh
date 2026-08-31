dcec800429765dc3cd7d3f6a2e6e8b95d05e4564
pavel-stastny <pavel.stastny@gmail.com>
2026-08-31 18:00:39 +0200

    SDNNT sync scripts

--- scripts/sdnnt_apply.sh
+++ scripts/sdnnt_apply.sh
@@ -1,13 +1,28 @@
-#! /bin/sh
+#!/bin/sh
 
-# Configuration   
+# Configuration
 # Fill in all the placeholders marked with TODO
 
-kramUrl="localhost:8088"
+kramUrl="http://localhost:8088"
+clientId="TODO"
+clientSecret="TODO"
+forwardedFor="TODO"
 
-curl -X GET $kramUrl/search/api/admin/v7.0/sdnnt/sync/batches \
-     -H "Content-Type: application/json" \
-     -H "X-Forwarded-For: TODO" 
+tokenResponse=$(
+  curl -s -X GET \
+    "$kramUrl/search/api/exts/v7.0/tokens/$clientId?secrets=$clientSecret"
+)
 
+accessToken=$(printf '%s' "$tokenResponse" | jq -r '.access_token')
 
+if [ -z "$accessToken" ] || [ "$accessToken" = "null" ]; then
+  echo "Failed to obtain access token"
+  echo "$tokenResponse"
+  exit 1
+fi
 
+curl -X POST "$kramUrl/search/api/admin/v7.0/processes" \
+  -H "Content-Type: application/json" \
+  -H "X-Forwarded-For: $forwardedFor" \
+  -H "Authorization: Bearer $accessToken" \
+  -d '{"defid":"sdnnt-sync","params":{}}'
